e09630917b59e4cc221f6a8d2efb31f0646dce1e
Adam Pingel <pingel@gmail.com>
2015-11-03 23:57:10 -0800

    comment out play expressions

--- shufldweb/app/routes/poker.js
+++ shufldweb/app/routes/poker.js
@@ -1,15 +1,36 @@
 import Ember from 'ember';
 
+
 export default Ember.Route.extend({
   model() {
     return {
       game: {
         label: "Some Poker Game",
-        twitter: "@pingel"
+        twitter: "@pingel",
+        players: [{}, {}]
       },
       player: {
       	description: "Player Foo"
-      }
+      },
+      state: {
+      	pot: 10,
+      	outcome: {},
+      	shared: [
+          {suit: "H", rank: "4"},
+          {suit: "H", rank: "5"},
+          {suit: "H", rank: "6"},
+          {suit: "H", rank: "7"},
+          {suit: "H", rank: "8"}]
+      },
+      sharedWithVisibility: [
+        [{suit: "H", rank: "4"}, false],
+        [{suit: "H", rank: "5"}, true],
+        [{suit: "H", rank: "6"}, true],
+        [{suit: "H", rank: "7"}, false],
+        [{suit: "H", rank: "8"}, false]]
     };
   }
+
+  // <!-- i lt model.state.numShown -->
+
 });
